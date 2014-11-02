edbc05357fbfcac23a4606589e32922d375a9be4
gklp <gokalpkuscu@gmail.com>
2014-11-02 17:14:59 +0200

    PHPDoc and Code Format

--- system/Criteria.php
+++ system/Criteria.php
@@ -1,118 +1,230 @@
 <?php
-class Criteria extends CriteriaBase{
-	public function __construct($model = null){
-		CriteriaBase::__construct($model);
-	}
 
-	public function where($column, $value){
-		if(empty($column) || !isset($value)){
-			throw new PipernateIllegalArgumentException(9006);
-		}else{
-			if($this->whereObject == null){
-				$this->whereObject = new Where();
-			}
-			$this->whereObject->addWhereClause($column, $value);
-		}
-	}
+/**
+ * Created by PhpStorm.
+ * Users: Gokalp Kuscu a.k.a gklp, Mikail Ozel a.k.a mike
+ * Date: 11/2/12
+ * Time: 4:03 PM
+ *
+ * This file automatically creates the sql queries and manages components of criteria api.
+ * If you will write sql extension, you must define in here and CriteriaBase for method chaining.
+ *
+ * Hold base object and then add clause it.
+ *
+ * @see sqlstatements directory
+ * @version 1.4
+ * @author gklp, mike
+ * @since 1.0
+ * @category core
+ *
+ */
+class Criteria extends CriteriaBase
+{
+    /**
+     * Model initialize for Criteria. So if model is null, it will throws a exception.
+     *
+     * @param null $model
+     */
+    public function __construct($model = null)
+    {
+        CriteriaBase::__construct($model);
+    }
 
-	public function limit($limitFrom, $limitCount){
-		if(!isset($limitFrom) || !isset($limitCount)){
-			throw new PipernateIllegalArgumentException(9007);
-		}else{
-			if($this->limitObject == null){
-				$this->limitObject = new Limit();
-			}
-			$this->limitObject->addLimitClause($limitFrom, $limitCount);
-		}
-	}
+    /**
+     * Where clause statement.
+     *
+     * @param $column
+     * @param $value
+     * @throws PipernateIllegalArgumentException
+     */
+    public function where($column, $value)
+    {
+        if (empty($column) || !isset($value)) {
+            throw new PipernateIllegalArgumentException(9006);
+        } else {
+            if ($this->whereObject == null) {
+                $this->whereObject = new Where();
+            }
+            $this->whereObject->addWhereClause($column, $value);
+        }
+    }
 
-	public function orderBy($column, $orderType = "ASC"){
-		if(empty($column)){
-			throw new PipernateIllegalArgumentException(9008);
-		}else{
-			if($this->orderObject == null){
-				$this->orderObject = new Order();
-			}
-			$this->orderObject->addOrderClause($column, $orderType);
-		}
-	}
+    /**
+     * Limit clause statement
+     *
+     * @param $limitFrom
+     * @param $limitCount
+     * @throws PipernateIllegalArgumentException
+     */
+    public function limit($limitFrom, $limitCount)
+    {
+        if (!isset($limitFrom) || !isset($limitCount)) {
+            throw new PipernateIllegalArgumentException(9007);
+        } else {
+            if ($this->limitObject == null) {
+                $this->limitObject = new Limit();
+            }
+            $this->limitObject->addLimitClause($limitFrom, $limitCount);
+        }
+    }
 
-	public function distinct($column, $alias = null){
-		if(empty($column)){
-			throw new PipernateIllegalArgumentException(9009);
-		} else{
-			if($this->distinctObject == null){
-				$this->distinctObject = new Distinct();
-			}
-			$this->distinctObject->addDistinctClause($column, $alias);
-		}
-	}
+    /**
+     * Order by clause statement. Order type default value is "ascending -> ASC".
+     * TODO: I think order by parameter must be enum (gklp)
+     *
+     * @param $column
+     * @param string $orderType
+     * @throws PipernateIllegalArgumentException
+     */
+    public function orderBy($column, $orderType = "ASC")
+    {
+        if (empty($column)) {
+            throw new PipernateIllegalArgumentException(9008);
+        } else {
+            if ($this->orderObject == null) {
+                $this->orderObject = new Order();
+            }
+            $this->orderObject->addOrderClause($column, $orderType);
+        }
+    }
 
-	public function count($column, $alias = null){
-		if(empty($column)){
-			throw new PipernateIllegalArgumentException(9010);
-		} else{
-			if($this->countObject == null){
-				$this->countObject = new Count();
-			}
-			$this->countObject->addCountClause($column, $alias);
-		}
-	}
+    /**
+     * Distinct clause statement
+     *
+     * @param $column
+     * @param null $alias
+     * @throws PipernateIllegalArgumentException
+     */
+    public function distinct($column, $alias = null)
+    {
+        if (empty($column)) {
+            throw new PipernateIllegalArgumentException(9009);
+        } else {
+            if ($this->distinctObject == null) {
+                $this->distinctObject = new Distinct();
+            }
+            $this->distinctObject->addDistinctClause($column, $alias);
+        }
+    }
 
-	public function sum($column, $alias = null){
-		if(empty($column)){
-			throw new PipernateIllegalArgumentException(9011);
-		} else{
-			if($this->sumObject == null){
-				$this->sumObject = new Sum();
-			}
-			$this->sumObject->addSumClause($column, $alias);
-		}
-	}
+    /**
+     * Count clause statement
+     *
+     * @param $column
+     * @param null $alias
+     * @throws PipernateIllegalArgumentException
+     */
+    public function count($column, $alias = null)
+    {
+        if (empty($column)) {
+            throw new PipernateIllegalArgumentException(9010);
+        } else {
+            if ($this->countObject == null) {
+                $this->countObject = new Count();
+            }
+            $this->countObject->addCountClause($column, $alias);
+        }
+    }
 
-	public function fields($columns, $alias = null){
-		if(empty($columns)){
-			throw new PipernateIllegalArgumentException(9012);
-		} else{
-			if($this->fieldsObject == null){
-				$this->fieldsObject = new Fields();
-			}
-			$this->fieldsObject->addFieldsClause($columns, $alias);
-		}
-	}
+    /**
+     * Sum clause statement
+     *
+     * @param $column
+     * @param null $alias
+     * @throws PipernateIllegalArgumentException
+     */
+    public function sum($column, $alias = null)
+    {
+        if (empty($column)) {
+            throw new PipernateIllegalArgumentException(9011);
+        } else {
+            if ($this->sumObject == null) {
+                $this->sumObject = new Sum();
+            }
+            $this->sumObject->addSumClause($column, $alias);
+        }
+    }
 
-	public function in($columnName, $inParameters){
-		if(empty($columnName) || empty($inParameters) ){
-			throw new PipernateIllegalArgumentException(9013);
-		}else{
-			if($this->whereObject == null){
-				$this->whereObject = new Where();
-			}
-			$this->whereObject->addInClause($columnName, $inParameters);
-		}
-	}
+    /**
+     * if you want to take same columns, you must define in here.
+     * TODO: parameters must be string array, do not use string parse.(gklp)
+     *
+     * @param $columns
+     * @param null $alias
+     * @throws PipernateIllegalArgumentException
+     */
+    public function fields($columns, $alias = null)
+    {
+        if (empty($columns)) {
+            throw new PipernateIllegalArgumentException(9012);
+        } else {
+            if ($this->fieldsObject == null) {
+                $this->fieldsObject = new Fields();
+            }
+            $this->fieldsObject->addFieldsClause($columns, $alias);
+        }
+    }
 
-	public function like($columnName, $likeParameter){
-		if(empty($columnName) || empty($likeParameter) ){
-			throw new PipernateIllegalArgumentException(9014);
-		}else{
-			if($this->whereObject == null){
-				$this->whereObject = new Where();
-			}
-			$this->whereObject->addLikeClause($columnName, $likeParameter);
-		}
-	}
+    /**
+     * In clause statement
+     *
+     * @param $columnName
+     * @param $inParameters
+     * @throws PipernateIllegalArgumentException
+     */
+    public function in($columnName, $inParameters)
+    {
+        if (empty($columnName) || empty($inParameters)) {
+            throw new PipernateIllegalArgumentException(9013);
+        } else {
+            if ($this->whereObject == null) {
+                $this->whereObject = new Where();
+            }
+            $this->whereObject->addInClause($columnName, $inParameters);
+        }
+    }
 
-	public function asModel(){
-		parent::setReturnType(ReturnType::AsModel);
-	}
+    /**
+     * Like clause statement.
+     *
+     * @param $columnName
+     * @param $likeParameter
+     * @throws PipernateIllegalArgumentException
+     */
+    public function like($columnName, $likeParameter)
+    {
+        if (empty($columnName) || empty($likeParameter)) {
+            throw new PipernateIllegalArgumentException(9014);
+        } else {
+            if ($this->whereObject == null) {
+                $this->whereObject = new Where();
+            }
+            $this->whereObject->addLikeClause($columnName, $likeParameter);
+        }
+    }
 
-	public function asArray(){
-		parent::setReturnType(ReturnType::AsArray);
-	}
-	
-	public function returnInsertId(){
-		parent::$returnInsertId = true;
-	}
+    /**
+     * You can change return type. Results return two different ways.
+     * Like Model or Array
+     */
+    public function asModel()
+    {
+        parent::setReturnType(ReturnType::AsModel);
+    }
+
+    /**
+     * You can change return type. Results return two different ways.
+     * Like Model or Array
+     */
+    public function asArray()
+    {
+        parent::setReturnType(ReturnType::AsArray);
+    }
+
+    public function returnInsertId()
+    {
+        parent::$returnInsertId = true;
+    }
 }
+
 ?>
--- system/core/CriteriaBase.php
+++ system/core/CriteriaBase.php
@@ -1,73 +1,87 @@
 <?php
-	class CriteriaBase{
-		private $model = null;
-		protected $returnType = ReturnType::AsModel;
-	
-		protected $fieldsObject;
-		protected $distinctObject;
-		protected $countObject;
-		protected $sumObject;
-		protected $whereObject;
-		protected $limitObject;
-		protected $orderObject;
-		
-		protected function __construct($model){
-		  if(empty($model)){
-			 throw new PipernateModelException(9001);
-			}
-		  $this->setModel($model);
-		}
-		
-		public function setModel($model){
-			if($model instanceof Model){
-				$this->model = $model;
-			}else{
-				if(!@class_exists($model)){
-					throw new PipernateModelException(9002);
-				}
-				$this->model =  new $model;
-			}
-		}
-		
-		public function getModel(){
-			return $this->model;
-		}
-	
-		protected function getWhereObject(){
-			return $this->whereObject;
-		}
-	
-		protected function getLimitObject(){
-			return $this->limitObject;
-		}
-	
-		protected function getOrderObject(){
-			return $this->orderObject;
-		}
-	
-		protected function getDistinctObject(){
-			return $this->distinctObject;
-		}
-		
-		protected function getCountObject(){
-			return $this->countObject;
-		}
-	
-		protected function getSumObject(){
-			return $this->sumObject;
-		}
-		
-	    protected function getFieldsObject(){
-			return $this->fieldsObject;
-		}
-		
-		protected function setReturnType($returnType){
-			$this->returnType = $returnType;
-		}
-		
-		public function getReturnType(){
-			return $this->returnType;
-		}
-	}
-	
+
+class CriteriaBase
+{
+    private $model = null;
+    protected $returnType = ReturnType::AsModel;
+
+    protected $fieldsObject;
+    protected $distinctObject;
+    protected $countObject;
+    protected $sumObject;
+    protected $whereObject;
+    protected $limitObject;
+    protected $orderObject;
+
+    protected function __construct($model)
+    {
+        if (empty($model)) {
+            throw new PipernateModelException(9001);
+        }
+        $this->setModel($model);
+    }
+
+    public function setModel($model)
+    {
+        if ($model instanceof Model) {
+            $this->model = $model;
+        } else {
+            if (!@class_exists($model)) {
+                throw new PipernateModelException(9002);
+            }
+            $this->model = new $model;
+        }
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    protected function getWhereObject()
+    {
+        return $this->whereObject;
+    }
+
+    protected function getLimitObject()
+    {
+        return $this->limitObject;
+    }
+
+    protected function getOrderObject()
+    {
+        return $this->orderObject;
+    }
+
+    protected function getDistinctObject()
+    {
+        return $this->distinctObject;
+    }
+
+    protected function getCountObject()
+    {
+        return $this->countObject;
+    }
+
+    protected function getSumObject()
+    {
+        return $this->sumObject;
+    }
+
+    protected function getFieldsObject()
+    {
+        return $this->fieldsObject;
+    }
+
+    protected function setReturnType($returnType)
+    {
+        $this->returnType = $returnType;
+    }
+
+    public function getReturnType()
+    {
+        return $this->returnType;
+    }
+}
+
 ?>
